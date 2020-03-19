44f61815d3c149c183c2730ba821e88fe651b2f9
Dongyun Tang <echo.jigsaw@gmail.com>
2020-03-19 17:54:01 +0200

    remove formal parm x

--- sqrt.scm
+++ sqrt.scm
@@ -1,15 +1,17 @@
 #lang sicp
 
 (define (sqrt x)
-  (define (good-enough? guess x)
+  (define (good-enough? guess)
     (< (abs (- (square guess) x)) 0.001))
   (define (square x) (* x x))
-  (define (improve guess x) (average guess (/ x guess)))
+  (define (improve guess) (average guess (/ x guess)))
   (define (average a b) (/ (+ a b) 2))
-  (define (sqrt-iter guess x)
-    (if (good-enough? guess x)
+  
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
         guess
-        (sqrt-iter (improve guess x) x)))
-  (sqrt-iter 1.0 x))
+        (sqrt-iter (improve guess))))
+  
+  (sqrt-iter 1.0))
 
 (sqrt 4)
