1b0348dbffbda5e46c379695188ed5fc992b6ce9
Dongyun Tang <echo.jigsaw@gmail.com>
2020-03-21 12:09:29 +0200

    add fib-iter

--- fib.scm
+++ fib.scm
@@ -5,4 +5,13 @@
         ((= n 1) 1)
         (else (+ (fib (- n 1)) (fib (- n 2))))))
 
-(fib 39)
+(define (fib-iter a b n)
+  (if (= n 1)
+      b
+     (fib-iter b (+ a b) (- n 1))))
+
+(define (fib2 n)
+  (fib-iter 0 1 n))
+
+(fib2 100)
+
