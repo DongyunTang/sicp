05fb474db89a5eb4e751b9aa76a1bfff8738be26
Dongyun Tang <echo.jigsaw@gmail.com>
2020-03-25 19:13:35 +0200

    exercise 1,19

--- fib.scm
+++ fib.scm
@@ -6,12 +6,31 @@
         (else (+ (fib (- n 1)) (fib (- n 2))))))
 
 (define (fib-iter a b n)
-  (if (= n 1)
+  (if (= n 0)
       b
-     (fib-iter b (+ a b) (- n 1))))
+     (fib-iter (+ a b) a (- n 1))))
+
+
+; (fib-iter 1 0 7)
 
 (define (fib2 n)
-  (fib-iter 0 1 n))
+  (fib-iter2 1 0 0 1 n))
+(define (fib-iter2 a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter2 a
+                   b
+                   (+ (square p) (square q))
+                   (+ (* 2 p q) (square q))
+                   (/ count 2)))
+        (else (fib-iter2 (+ (* b q) (* a q) (* a p))
+                         (+ (* b p) (* a q))
+                         p
+                         q
+                         (- count 1)))))
 
-(fib2 100)
+(define (square p) (* p p))
+
+(fib2 7)
+
 
